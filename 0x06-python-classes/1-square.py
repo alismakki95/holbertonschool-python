20a25569e019511e4cd9e778fd23939499c6516b
alismakki95 <69826485+alismakki95@users.noreply.github.com>
2020-09-19 19:48:42 +0300

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+"""python3 -c 'print(__import__("my_module").__doc__)'"""
+
+
 class Square:
+    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
     __size = 0
 
     def __init__(self, new_size=0):
+        """python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'"""
         """Initialize class."""
         if new_size is not 0:
             self.__size = new_size
