0790e9bab6b6f0e70fee4d041fb12e6b4d4efd98
alismakki95 <69826485+alismakki95@users.noreply.github.com>
2020-09-19 19:35:45 +0300

    Update 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 """ python3 -c 'print(__import__("my_module").__doc__)'"""
 class Square:
+"""python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
     pass  # An empty block
