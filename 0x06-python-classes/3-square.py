130857ad22b4e13ac6eea1959ef239cee15325c2
alismakki95 <69826485+alismakki95@users.noreply.github.com>
2020-09-19 20:06:58 +0300

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 """python3 -c 'print(__import__("my_module").__doc__)'"""
+
+
 class Square:
   """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
     __size = 0
+     def __init__(self, size=0):
+        """Initialize class."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Square area."""
+        return (self.__size * self.__size)
