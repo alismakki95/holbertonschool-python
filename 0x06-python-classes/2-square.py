b10348904edacd02755b6a557bc7025dd1793817
alismakki95 <69826485+alismakki95@users.noreply.github.com>
2020-09-19 20:17:42 +0300

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
-
 """python3 -c 'print(__import__("my_module").__doc__)'"""
+
+
 class Square:
     """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
     __size = 0
@@ -15,3 +15,4 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+        
